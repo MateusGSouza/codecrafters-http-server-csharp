d15371b886cf722259e514100ccc6242fea00826
Mateus Souza <115435321+MateusGSouza@users.noreply.github.com>
2024-04-10 02:50:16 -0300

    Pass 2nd stage

--- src/Server.cs
+++ src/Server.cs
@@ -1,10 +1,57 @@
+using System.Linq.Expressions;
 using System.Net;
 using System.Net.Sockets;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
+class Server
+{
+    static void Main(string[] args)
+    {
+        TcpListener server = null;
+        try
+        {
+            server = new TcpListener(IPAddress.Any, 4221);
+            server.Start();
 
-// Uncomment this block to pass the first stage
-TcpListener server = new TcpListener(IPAddress.Any, 4221);
-server.Start();
-server.AcceptSocket(); // wait for client
+            Console.WriteLine("Started server at port 4221");
+            Byte[] bytes = new Byte[256];
+            String data = null;
+
+            while(true)
+            {
+                using TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+
+                data = null;
+
+                NetworkStream stream = client.GetStream();
+
+                int i;
+
+                while((i = stream.Read(bytes, 0, bytes.Length))!=0)
+                {
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+
+                    // data = data.ToUpper();
+                    data = "HTTP/1.1 200 OK\r\n\r\n"
+
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine("Sent: {0}", data);
+                }
+            }
+            }
+        catch(SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+        finally
+        {
+            server.Stop();
+        }
+
+    }
+}
+
+// TODO send TCP request
